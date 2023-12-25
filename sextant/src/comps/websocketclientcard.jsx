a91ecefae21900dcc57b3c4d4d58ef5100230440
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-12-25 13:27:37 +0530

    idk why, i'd run away, wohoo hoo hooooo

--- sextant/src/comps/websocketclientcard.jsx
+++ sextant/src/comps/websocketclientcard.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Card from './card.jsx';
-let c = 0;
+// let c = 0;
 
 function WSCC() {
   const [latency, setLatency] = useState(-1);
   
   useEffect(() => {
-    if (c > 0) return; // Only run once (on mount
-    c = 1;
+    // if (c > 0) return; // Only run once (on mount
+    // c = 1;
     const client = new WebSocket('ws://localhost:55455/');
 
     client.onmessage = function (e) {
